ecf354854c1008da9dff306402eff18da7e4e15a
wyc <wyc@fj6688.com>
2026-09-06 10:57:51 +0800

    Restore Lucky module and disable upstream update comparison

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "author": "hwyc888",
-    "description": "Lucky Merlin 自维护版：端口转发/DDNS/Web服务/Stun内网穿透/网络唤醒/计划任务/ACME自动证书/网络存储",
-    "home_url": "Module_lucky_melin.asp",
-    "link": "https://github.com/hwyc888/lucky-melin",
-    "md5": "58f49e79fa7427ccf34b3b1bcdbf0b97",
-    "module": "lucky_melin",
+    "author": "vj23456, kiritoknight",
+    "description": "端口转发/DDNS/Web服务/Stun内网穿透/网络唤醒/计划任务/ACME自动证书/网络存储",
+    "home_url": "Module_lucky.asp",
+    "link": "https://github.com/vj23456/MerlinLucky",
+    "md5": "7dc23de8dd33330e75c4984c0f3e6776",
+    "module": "lucky",
     "tags": "工具",
-    "title": "Lucky Merlin",
-    "version": "1.6.3"
+    "title": "Lucky",
+    "version": "1.6.4"
 }
--- lucky/uninstall.sh
+++ lucky/uninstall.sh
@@ -10,11 +10,11 @@ fi
 find /koolshare/init.d/ -name "*lucky*" | xargs rm -rf
 rm -rf /koolshare/bin/lucky 2>/dev/null
 rm -rf /tmp/lucky 2>/dev/null
-rm -rf /koolshare/res/icon-lucky.png /koolshare/res/icon-lucky_melin.png 2>/dev/null
+rm -rf /koolshare/res/icon-lucky.png 2>/dev/null
 rm -rf /koolshare/scripts/lucky*.sh 2>/dev/null
-rm -rf /koolshare/webs/Module_lucky.asp /koolshare/webs/Module_lucky_melin.asp 2>/dev/null
+rm -rf /koolshare/webs/Module_lucky.asp 2>/dev/null
 rm -rf /koolshare/scripts/lucky_install.sh 2>/dev/null
-rm -rf /koolshare/scripts/uninstall_lucky.sh /koolshare/scripts/uninstall_lucky_melin.sh 2>/dev/null
+rm -rf /koolshare/scripts/uninstall_lucky.sh 2>/dev/null
 rm -rf /koolshare/configs/lucky 2>/dev/null
 rm -rf /tmp/upload/lucky* 2>/dev/null
 rm -f /var/lock/lucky.lock /tmp/lucky.pid /tmp/var/lucky.pid 2>/dev/null
@@ -29,12 +29,6 @@ dbus remove lucky_reset_port
 dbus remove lucky_reset_safeurl
 dbus remove lucky_reset_user
 dbus remove lucky_safeurl
-dbus remove softcenter_module_lucky_melin_name
-dbus remove softcenter_module_lucky_melin_install
-dbus remove softcenter_module_lucky_melin_version
-dbus remove softcenter_module_lucky_melin_title
-dbus remove softcenter_module_lucky_melin_description
-# 同时清理 1.6.2 及更早版本遗留的官方 lucky 模块身份。
 dbus remove softcenter_module_lucky_name
 dbus remove softcenter_module_lucky_install
 dbus remove softcenter_module_lucky_version
